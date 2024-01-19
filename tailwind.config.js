ede4eb5f31dc38b7a15179565a9871dda9eeca71
ibrohimjon <moydinovibrohimjon2530@gmail.com>
2024-01-19 17:27:47 +0300

    project 3 section

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      margin:{
+        '29':"29px"
+      },
+      padding:{
+        '29':'29px'
+      },
+      colors:{
+        "abdulvahob":"#757574"
+      },
+    },
+  },
+  plugins: [],
+}
+
